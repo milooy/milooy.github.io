1261264d0119ec9281748f6a9f44c17320ed6da1
joshua1988 <jangkeehyo@gmail.com>
2022-08-10 17:57:19 +0900

    feats: add liberation mono font to code blocks

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,6 +5,8 @@ import "typeface-merriweather"
 import "./src/normalize.css"
 // custom CSS styles
 import "./src/style.css"
+import "./src/font.css"
 
 // Highlighting for code blocks
-import "prismjs/themes/prism.css"
+// import "prismjs/themes/prism.css"
+import "prism-theme-night-owl";
