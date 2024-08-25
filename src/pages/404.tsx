00cefeb73023171551991db53fa09b2db38b6e1c
yurim <yurim@toss.im>
2024-08-25 16:55:20 +0900

    remove package.lock

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,5 +1,5 @@
-import * as React from "react"
 import { graphql, PageProps } from "gatsby"
+import * as React from "react"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
